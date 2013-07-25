f4c516b622e24b8548fedfa6993488d8ef4faf4a
Bogdan Hodorog <bogdan.hodorog@3pillarglobal.com>
2013-07-25 14:54:21 +0300

    Init repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parent",
+    version="0.1",
+    install_requires=["requests==1.2.3", "child", ],
+    dependency_links=[r'git+ssh://git@github.com/bhodorog/child_prj.git#egg=child-prj-0.1', ],
+    packages=find_packages(),
+)
+
