e3a96067a27684b10aef4a74cb11a2bd3a2531c3
Bogdan Hodorog <bogdan.hodorog@3pillarglobal.com>
2013-09-20 15:00:08 +0300

    depends of an older requests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="parent",
     version="0.1",
-    install_requires=["requests==1.2.3", "child", ],
+    install_requires=["requests==1.0.0", "child", ],
     dependency_links=[r'git+ssh://git@github.com/bhodorog/child_prj.git#egg=child-prj-0.1', ],
     packages=find_packages(),
 )
